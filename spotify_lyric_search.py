7cd7a87287783c0c5b51f519ded940cd3eb17335
danny <dannyzamora1996@gmail.com>
2021-02-04 20:48:18 -0800

    small details

--- spotify_lyric_search.py
+++ spotify_lyric_search.py
@@ -15,24 +15,8 @@ class SpotifyLyricSearch:
         for hit in request['hits']:
             print(hit['result']['full_title'],'\n') 
 
-#for hit in request['sections'][0]['hits']:
-   # print(hit['result']['title'])# import requests
-
-
-
-# url = "http://api.genius.com/search?q=Kendrick%20Lamar"
-
-# payload={}
-# headers = {
-#   'Authorization': 'Bearer',
-#   'Cookie': '__cfduid=d40ec58ce31c0da814768a95202a215ca1612483091'
-# }
-
-# response = requests.request("GET", url, headers=headers, data=payload)
-# print(response.text)
-
 if __name__ == '__main__':
-    #
-    sls = SpotifyLyricSearch("my girl")
+    sls = SpotifyLyricSearch(input("Enter Lyrics please: "))
+    print('/n')
     sls.find_song()
     
